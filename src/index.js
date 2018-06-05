f516e56a812515a89790e0dd88cc13508252b168
Ahamed <ahamedali95@gmail.com>
2018-06-05 15:24:04 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-// Code Goes Here
+import React from 'react' // We need to import react so we can make use of its .component class
+import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+
+class Order extends React.Component {
+  render() {
+  return (
+    <div className="order">
+      <ul>
+        <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
+        <li>{this.props.size}</li>
+        <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
+        <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
+      </ul>
+    </div>
+  );
+}
+}
+
+// Order.defaultProps = {
+//   isCone: true,
+//   size: "regular",
+// };
+
+ReactDOM.render(
+  <Order />,
+  document.getElementById("root")
+);
